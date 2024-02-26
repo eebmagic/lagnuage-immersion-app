5c969585edd2ba06383e89c66b250442f8c28d36
E Bolton <eebmagic@gmail.com>
2024-02-25 22:19:23 -0500

    add ingestion file

---
+++ ingest.py
@@ -0,0 +1,123 @@
+import time
+import math
+import re
+import json
+from tqdm import tqdm
+
+from googletrans import Translator
+import spacy
+
+translator = Translator()
+nlp = spacy.load('pt_core_news_sm')
+
+
+def lemmatize(text):
+    '''
+    Default lemmatize function, using spacy.
+    '''
+    def realWord(text):
+        # Check that a string isn't just punctuation
+        return re.match(r'[^\w\s]', text) == None
+
+    doc = nlp(text)
+    lemmas = []
+    for word in doc:
+        lemma = word.lemma_
+        if realWord(lemma) and lemma not in lemmas:
+            lemmas.append(lemma.lower())
+
+    return lemmas
+
+
+def translate(text, src='pt', dest='en'):
+    '''
+    Default translator function, using googletrans (a free python wrapper for Google Translate).
+    This can be swapped out for a different translator if desired.
+    '''
+    translationResult = translator.translate(text, src=src, dest=dest)
+
+    return translationResult.text
+
+
+def prepChunk(items, chunkString=''):
+    # Pull text from items
+    sents = [item['text'] for item in items]
+
+    # Generate new data
+    print(f"Getting translations: {chunkString}")
+    # translations = [translate(sent, src='pt', dest='en') for sent in sents]
+    # translations = [translate(sent, src='pt', dest='en') for sent in tqdm(sents)]
+    # translations = []
+    # for sent in tqdm(sents):
+    #     translations.append(translate(sent, src='pt', dest='en'))
+
+
+    # lemmas = [lemmatize(sent) for sent in sents]
+
+    # Combine into new data
+    result = []
+    # for item, translation, lemma in zip(items, translations, lemmas):
+    for item in tqdm(items):
+        try:
+            out = item.copy()
+            translation = translate(item['text'], src='pt', dest='en')
+            lemma = lemmatize(item['text'])
+            out['trans'] = translation
+            out['lemmas'] = lemma
+            result.append(out)
+        except Exception as e:
+            print(f"Error processing item: {e}")
+            print(f"Skipping item")
+
+    
+    return result
+
+
+def ingestAll(items, sourceType, sourcePath, chunkSize=10, chunkDelay=0):
+    result = {}
+    totalChunks = math.ceil(len(items) / chunkSize)
+    for i in range(0, len(items), chunkSize):
+        chunk = items[i:i+chunkSize]
+        chunkIndex = i // chunkSize
+        try:
+            preppedChunk = prepChunk(chunk, chunkString=f"{chunkIndex} / {totalChunks}")
+            for item in preppedChunk:
+                item['source_type'] = sourceType
+                item['source_path'] = sourcePath
+                result[item['id']] = item
+
+            if chunkDelay:
+                print(f"Sleeping for {chunkDelay} seconds...")
+                time.sleep(chunkDelay)
+
+        except KeyboardInterrupt:
+            print(f"Caught KeyboardInterrupt, stopping ingestion")
+            break
+
+        except Exception as e:
+            print(f"Error processing chunk {chunkIndex} - {totalChunks}: {e}")
+            print(f"Skipping chunk")
+
+
+
+    return result
+
+
+def ingestNew(items, sourceType, sourcePath, entriesSource='./entries.json', chunkSize=10, chunkDelay=0):
+    # Load existing entries
+    with open(entriesSource) as file:
+        existingEntries = json.load(file)
+        print(f"Got {len(existingEntries)} existing entries")
+    
+    # Filter out existing entries
+    newEntries = list(filter(lambda x: x['id'] not in existingEntries, items))
+    print(f"Found {len(newEntries)} / {len(items)} to be new entries")
+
+    # Ingest new entries
+    processedResult = ingestAll(newEntries, sourceType, sourcePath, chunkSize, chunkDelay)
+
+    # Update in file
+    existingEntries.update(processedResult)
+    with open('entries.json', 'w') as file:
+        json.dump(existingEntries, file)
+        print(f"Updated entries file with {len(existingEntries)} total entries ({len(processedResult)} are new)")
