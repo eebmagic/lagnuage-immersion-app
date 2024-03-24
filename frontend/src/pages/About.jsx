b2eae83f24a9068edbb1e9d16f17b05ee2d72cf6
E Bolton <eebmagic@gmail.com>
2024-03-23 23:32:31 -0400

    minimal code to call endpoint on server for snippets

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,11 +1,25 @@
 import { Link } from "react-router-dom";
 
 export function About() {
-    console.log(`RUNING ABOUT FUNCTION`);
-    return (
-        <div>
-          <h1>ABOUT</h1>
-          <Link to="/">Home</Link>
-        </div>
-    )
+  const apiURL = import.meta.env.VITE_API_URL;
+
+  console.log(`RUNING ABOUT FUNCTION`);
+
+  console.log(`Making api call to: ${apiURL}/snippets`);
+
+  fetch(`http://${apiURL}/snippets`)
+    .then((response) => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.error('There was an error!', error);
+    })
+
+  return (
+    <div>
+      <h1>ABOUT</h1>
+      <Link to="/">Home</Link>
+    </div>
+  )
 }
--- server/server.py
+++ server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymongo
 import random
 
 app = Flask(__name__)
+CORS(app)
 
 client = pymongo.MongoClient('mongodb://localhost:27017/')
 vocab = client['language']['vocab']
