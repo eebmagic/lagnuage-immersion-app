bbcbbbc1fbcea5fc99bd4cfb02f752cea95b21d4
E Bolton <eebmagic@gmail.com>
2024-03-26 02:54:30 -0400

    add menubar with navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
-import { Routes, Route, Outlet, Link } from 'react-router-dom';
+import { Routes, Route, Outlet, Link, useNavigate } from 'react-router-dom';
+
+import { Menubar } from 'primereact/menubar';
+import "primereact/resources/themes/lara-dark-blue/theme.css";
+import "primeicons/primeicons.css";
+
 import { About } from './pages/About';
 import { Learn } from './pages/Learn';
 
-import "primereact/resources/themes/lara-dark-blue/theme.css";
-import "primeicons/primeicons.css";
 
 function App() {
     return (
@@ -13,16 +16,34 @@ function App() {
                 <Route index element={<Hello />} />
                 <Route path="learn" element={<Learn />} />
                 <Route path="about" element={<About />} />
-
             </Route>
         </Routes>
     )
 }
 
 function PrimaryLayout() {
+    const navigateTo = useNavigate();
+
+    const items = [
+        {
+            label: 'Home',
+            icon: 'pi pi-fw pi-home',
+            command: () => navigateTo('/')
+        },
+        {
+            label: 'Learn',
+            icon: 'pi pi-fw pi-book',
+            command: () => navigateTo('/learn')
+        },
+        {
+            label: 'About',
+            icon: 'pi pi-fw pi-info-circle',
+            command: () => navigateTo('/about')
+        }
+    ];
     return (
         <div>
-            {/* Add primary components here. Such as header bars, etc. */}
+            <Menubar model={items} />
             <h1>Layout</h1>
             <Outlet />
         </div>
