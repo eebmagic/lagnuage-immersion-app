d79b57ffb0f85a209f99c70784e51c6b7f68f612
E Bolton <eebmagic@gmail.com>
2024-03-26 19:06:21 -0400

    linting corrections

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,56 +10,55 @@ import { Learn } from './pages/Learn';
 
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<PrimaryLayout />}>
-                <Route index element={<Hello />} />
-                <Route path="learn" element={<Learn />} />
-                <Route path="about" element={<About />} />
-            </Route>
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<PrimaryLayout />}>
+        <Route index element={<Hello />} />
+        <Route path="learn" element={<Learn />} />
+        <Route path="about" element={<About />} />
+      </Route>
+    </Routes>
+  )
 }
 
 function PrimaryLayout() {
-    const navigateTo = useNavigate();
+  const navigateTo = useNavigate();
 
-    const items = [
-        {
-            label: 'Home',
-            icon: 'pi pi-fw pi-home',
-            command: () => navigateTo('/')
-        },
-        {
-            label: 'Learn',
-            icon: 'pi pi-fw pi-book',
-            command: () => navigateTo('/learn')
-        },
-        {
-            label: 'About',
-            icon: 'pi pi-fw pi-info-circle',
-            command: () => navigateTo('/about')
-        }
-    ];
-    return (
-        <div>
-            <Menubar model={items} />
-            <h1>Layout</h1>
-            <Outlet />
-        </div>
-    )
+  const items = [
+    {
+      label: 'Home',
+      icon: 'pi pi-fw pi-home',
+      command: () => navigateTo('/')
+    },
+    {
+      label: 'Learn',
+      icon: 'pi pi-fw pi-book',
+      command: () => navigateTo('/learn')
+    },
+    {
+      label: 'About',
+      icon: 'pi pi-fw pi-info-circle',
+      command: () => navigateTo('/about')
+    }
+  ];
+  return (
+    <div>
+      <Menubar model={items} />
+      <h1>Layout</h1>
+
+      <Outlet />
+    </div>
+  )
 }
 
 function Hello() {
-    return (
-        <div>
-          <h1>HELLO WORLD (from App.jsx)</h1>
-          <Link to="/learn">Learn</Link>
-          <Link to="/about">About</Link>
-        </div>
-    )
-
+  return (
+    <div>
+      <h1>HELLO WORLD (from App.jsx)</h1>
+      <Link to="/learn">Learn</Link>
+      <Link to="/about">About</Link>
+    </div>
+  )
 }
-
 
 export default App;
