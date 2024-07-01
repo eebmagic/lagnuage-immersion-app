9eb04869847c1c34a6803883ba2cf6011b51146a
E Bolton <eebmagic@gmail.com>
2024-07-01 00:09:08 -0400

    add drop down with word-specific selection Add file for button options

--- frontend/src/components/Snippet.jsx
+++ frontend/src/components/Snippet.jsx
@@ -1,19 +1,133 @@
+import { useState } from 'react';
 import { Card } from 'primereact/card';
 import { Accordion, AccordionTab } from 'primereact/accordion';
+import { Button } from 'primereact/button';
 
-export function Snippet({ snippet, index }) {
+import { DiffGrid } from './DiffGrid';
+import { useUser } from "../UserContext.jsx";
+
+const API_URL = import.meta.env.VITE_API_URL;
+const POST_REP_API_PATH = `http://${API_URL}/rep`;
+export const buttonOptions = {
+  'Again': {
+    difficulty: 'Again',
+    severity: 'danger',
+  },
+  'Hard': {
+    difficulty: 'Hard',
+    severity: 'warning',
+  },
+  'Good':{
+    difficulty: 'Good',
+    severity: 'info',
+    color: '#e3e352',
+  },
+  'Easy': {
+    difficulty: 'Easy',
+    severity: 'success',
+  }
+};
+
+export function Snippet({
+  snippet,
+  moveIndex,
+}) {
   const mediaTitle = snippet.source_path.split('/').pop().split('.')[0];
   const subTitle = `page ${snippet.page} : sentence ${snippet.page_sentence_index}`;
 
+  const { user } = useUser();
+  const updateFunc = (vocabList, difficulty, shouldIncrement) => {
+    const username = user.username;
+
+    const payload = {
+      strength: difficulty.toLowerCase(),
+      review_time: Date.now() / 1000,
+      vocab: vocabList,
+    }
+    // console.log(vocabList)
+    console.log(payload)
+
+    const fullUrl = `${POST_REP_API_PATH}?username=${username}`
+    console.log(`Sending payload for difficulty ${difficulty}: incrementing: ${shouldIncrement}`);
+    fetch(fullUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(payload),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        if (response.headers.get('Content-Type') !== 'application/json') {
+          throw new Error('Content-Type is not application/json');
+        };
+
+        var all200 = false;
+        var data = null;
+        if (response.status == 207) {
+          data = await response.json();
+          all200 = data.codes ?
+            data.codes.every((code) => (code >= 200) && (code < 300))
+            : false;
+        }
+        
+        console.log(response.status == 200, response.status == 204, all200);
+        if (response.status == 200 || response.status == 204 || all200) {
+          // Increment the index if a good response comes back
+          if (shouldIncrement) {
+            moveIndex(1);
+          }
+        } else {
+          console.log(`Got back this response: ${response.status}`);
+          console.log(response);
+          if (!data) {
+            data = await response.json();
+          }
+          console.log('data', data);
+        }
+      })
+      .catch((error) => {
+        console.error(payload, 'There was a problem with your fetch operation:', error);
+      })
+  }
+
+  const [accordionIndex, setAccordionIndex] = useState([1]);
+
   return (
-    <div key={index}>
+    <div key={snippet.id}>
       <Card title={mediaTitle} subTitle={subTitle}>
-        <p>{snippet.text}</p>
-        <Accordion>
-          <AccordionTab header="Translation">
-            <p>{snippet.translation}</p>
-          </AccordionTab>
-        </Accordion>
+        <div className="card flex flex-column gap-0">
+          <p>{snippet.text}</p>
+          <Accordion multiple activeIndex={accordionIndex} onTabChange={(e) => setAccordionIndex(e.index)}>
+            <AccordionTab header="Translation">
+              <p>{snippet.translation}</p>
+            </AccordionTab>
+            <AccordionTab header="Specific Word Difficulty">
+              <DiffGrid
+                snippet={snippet}
+                updateFunc={updateFunc}
+              />
+            </AccordionTab>
+          </Accordion>
+
+          {/* Buttons to increment */}
+          <div className="card flex flex-column gap-3">
+            <div className="flex flex-wrap justify-content-center gap-3">
+              {Object.values(buttonOptions).map(({ difficulty, severity, color}) => (
+                <Button
+                  key={difficulty}
+                  label={difficulty}
+                  severity={severity}
+                  disabled={accordionIndex.includes(1)}
+                  style={color ? { backgroundColor: color, borderColor: color, color: 'white' } : {}}
+                  onClick={() => updateFunc(snippet.contained_vocab, difficulty, true)}
+                />
+              ))}
+            </div>
+          </div>
+        </div>
       </Card>
     </div>
   )
--- frontend/src/components/Stack.jsx
+++ frontend/src/components/Stack.jsx
@@ -1,71 +1,10 @@
 import { Snippet } from './Snippet';
 import { useState } from 'react';
-import { Button } from 'primereact/button';
-
-import { useUser } from "../UserContext.jsx";
-
-const API_URL = import.meta.env.VITE_API_URL;
-const POST_REP_API_PATH = `http://${API_URL}/rep`;
-
-const buttonOptions = [
-  {
-    label: 'Again',
-    severity: 'danger',
-  },
-  {
-    label: 'Hard',
-    severity: 'warning',
-  },
-  {
-    label: 'Good',
-    severity: 'info',
-  },
-  {
-    label: 'Easy',
-    severity: 'success',
-  }
-];
 
 export function Stack({ snippets }) {
   const [index, setIndex] = useState(0);
-  const { user } = useUser();
-
-  const logButtonFunc = (snippet, label, username) => {
-    const payload = {
-      strength: label.toLowerCase(),
-      review_time: Date.now() / 1000,
-      vocab: snippet.contained_vocab,
-    }
-
-    // post the payload to to 
-    const fullUrl = `${POST_REP_API_PATH}?username=${username}`
-    fetch(fullUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        if (response.status == 200) {
-          // Increment the index if a good response comes back
-          setIndex(index + 1);
-        } else {
-          console.log(`Got back this response: ${response.status}`);
-          console.log(response);
-          response.json().then(data => {
-            console.log(`Got back this response data as json:`);
-            console.log(data);
-          })
-        }
-      })
-      .catch((error) => {
-        console.error('There was a problem with your fetch operation:', error);
-      })
+  const moveIndex = (increment) => {
+    setIndex(index + increment);
   };
 
   return (
@@ -75,24 +14,12 @@ export function Stack({ snippets }) {
         { (snippets.length > 0) && (index < snippets.length) ? (
             <div className="flex flex-column gap-2">
               <h2>{index+1} / {snippets.length}</h2>
-              <Snippet snippet={snippets[index]} index={index} />
+              <Snippet
+                key={index}
+                snippet={snippets[index]}
+                moveIndex={moveIndex}
+              />
 
-              <div className="card flex flex-wrap justify-content-center gap-3">
-                {buttonOptions.map(({ label, severity }) => (
-                  <Button
-                    key={label}
-                    label={label}
-                    severity={severity}
-                    onClick={() => logButtonFunc(snippets[index], label, user.username)}
-                  />
-                ))}
-              </div>
-              <div className='card flex flex-wrap justify-content-center gap-3'>
-                <Button label='Again' severity='danger' />
-                <Button label='Hard' severity='warning' />
-                <Button label='Good' style={{ backgroundColor: '#8c9234', borderColor: '#8c9234', color: 'white' }} />
-                <Button label='Easy' severity='success' />
-              </div>
             </div>
           ) : (
             <div>
