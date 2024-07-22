384e6fc2a4b85764d568dc1d0eac3361efc2e7db
E Bolton <eebmagic@gmail.com>
2024-07-22 08:17:43 -0400

    lint corrections: clean.py

--- ingestion/clean.py
+++ ingestion/clean.py
@@ -1,43 +1,56 @@
+'''
+Collection of helper functions to help with prepping text for translation.
+'''
 import re
-from nltk.tokenize import sent_tokenize
 from hashlib import sha256
+from nltk.tokenize import sent_tokenize
 
 def check_line(line):
     '''
     check with regex if line is just a page number
     '''
     result = re.match(r'^(page |pg )?\d+$', line)
-    return result == None
+    return result is None
 
 
 def clean(line):
+    '''
+    remove leading symbol from line
+    '''
     if line.startswith('- '):
         line = line[2:]
 
     return line
 
 def process_lines(lines):
+    '''
+    Get the cleaned lines of text and page number for a page.
+    '''
     out = []
-    N = 3
-    pageNum = None
+    n = 3
+    page_num = None
     for i, line in enumerate(lines):
         line = line['text']
-        if i < N or i > len(lines) - N:
+        if i < n or i > len(lines) - n:
             if check_line(line):
                 out.append(line)
             else:
-                pageNum = line.strip()
+                page_num = line.strip()
 
         else:
             out.append(line)
 
     cleaned = list(map(clean, out))
 
-    return cleaned, pageNum
+    return cleaned, page_num
 
 
 def split_sentences(lines, numbers):
-    def getMostFreq(nums):
+    '''
+    Split lines of text from pdf to sentences.
+    Generates indexing data in the process.
+    '''
+    def get_most_freq(nums):
         counts = {}
         for num in nums:
             if num not in counts:
@@ -51,47 +64,46 @@ def split_sentences(lines, numbers):
 
         if nums[0]:
             return nums[0]
-        else:
-            return '-1'
 
+        return '-1'
 
     out = []
     buffer = ""
-    numBuffer = []
-    pageSentCounts = {}
-    sentenceCounter = 0
+    num_buffer = []
+    page_sent_counts = {}
+    sentence_counter = 0
     for line, num in zip(lines, numbers):
         buffer = (buffer + ' ' + line).strip()
-        numBuffer.append(num)
+        num_buffer.append(num)
         sents = sent_tokenize(buffer, language='portuguese')
         if len(sents) > 1:
             for sent in sents[:-1]:
-                properPage = getMostFreq(numBuffer)
-                pageCount = pageSentCounts.get(properPage, 0)
-                pageSentCounts[properPage] = pageCount + 1
+                proper_page = get_most_freq(num_buffer)
+                page_count = page_sent_counts.get(proper_page, 0)
+                page_sent_counts[proper_page] = page_count + 1
                 out.append({
                     'id': sha256(sent.encode('utf-8')).hexdigest(),
-                    'page': properPage,
-                    'page_sentence_index': pageCount,
-                    'combined_index': f'{properPage}.{pageCount}',
-                    'media_index': sentenceCounter,
+                    'page': proper_page,
+                    'page_sentence_index': page_count,
+                    'combined_index': f'{proper_page}.{page_count}',
+                    'media_index': sentence_counter,
                     'text': sent,
                 })
-                sentenceCounter += 1
+                sentence_counter += 1
             buffer = sents[-1]
-            numBuffer = [numBuffer[-1]]
+            num_buffer = [num_buffer[-1]]
 
     # TODO: Having this extra iteration outside the loop is a STRONG code smell.
     # Get the last sentence from leftover buffers
-    properPage = getMostFreq(numBuffer)
-    pageCount = pageSentCounts.get(properPage, 0)
-    pageSentCounts[properPage] = pageCount + 1
+    proper_page = get_most_freq(num_buffer)
+    page_count = page_sent_counts.get(proper_page, 0)
+    page_sent_counts[proper_page] = page_count + 1
     out.append({
         'id': sha256(buffer.encode('utf-8')).hexdigest(),
-        'page': getMostFreq(numBuffer),
-        'page_sentence_index': pageCount,
-        'combined_index': f'{properPage}.{pageCount}',
-        'media_index': sentenceCounter,
+        'page': get_most_freq(num_buffer),
+        'page_sentence_index': page_count,
+        'combined_index': f'{proper_page}.{page_count}',
+        'media_index': sentence_counter,
         'text': buffer,
     })
 
