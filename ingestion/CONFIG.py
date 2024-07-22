5a128973a486c78e149c9fadfb27a1f3e00cd08b
E Bolton <eebmagic@gmail.com>
2024-07-22 08:17:36 -0400

    lint corrections: config.py

--- ingestion/CONFIG.py
+++ ingestion/CONFIG.py
@@ -1,7 +1,15 @@
-# from translators import DeepL
-# PreferredTranslator = DeepL()
+'''
+Set some constants to be used throughout the ingestion process here.
+
+PreferredTranslator: The translation class to use when generating translations.
+
+PreferredInterface: Where to store the translation results.
+'''
 from translators import DummyTranslator
+# from translators import DeepL
+from interfaces import MongoInterface
+
+# PreferredTranslator = DeepL()
 PreferredTranslator = DummyTranslator()
 
-from interfaces import MongoInterface
 PreferredInterface = MongoInterface('mongodb://localhost:27017/')
